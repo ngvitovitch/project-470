fdd63286ed082fffb6bf859dbf527d38e189f43e
Caleb Everett <everett.caleb@gmail.com>
2013-04-02 23:36:55 -0400

    Simplify events routing
    Moved events routing from the dwelling scope.
    
    Added checks so users can only retrieve events of their dwelling.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,10 @@
 class EventsController < ApplicationController
+  before_filter :get_dwelling_and_event
+
 
   # GET /events
   # GET /events.json
   def index
-    @dwelling = Dwelling.find params[:dwelling_id]
     @events = @dwelling.events.order(:date)
 
     respond_to do |format|
@@ -15,8 +16,6 @@ class EventsController < ApplicationController
   # GET /events/1
   # GET /events/1.json
   def show
-    @event = Event.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @event }
@@ -26,7 +25,6 @@ class EventsController < ApplicationController
   # GET /events/new
   # GET /events/new.json
   def new
-    @dwelling = Dwelling.find params[:dwelling_id]
     @event = @dwelling.events.build
     @event.date = Time.now
 
@@ -38,19 +36,17 @@ class EventsController < ApplicationController
 
   # GET /events/1/edit
   def edit
-    @event = Event.find(params[:id])
   end
 
   # POST /events
   # POST /events.json
   def create
-    @dwelling = Dwelling.find params[:dwelling_id]
     @event = @dwelling.events.build(params[:event])
     @event.user = current_user
 
     respond_to do |format|
       if @event.save
-        format.html { redirect_to dwelling_event_path(@event.dwelling, @event), notice: 'Event was successfully created.' }
+        format.html { redirect_to event_path(@event), notice: 'Event was successfully created.' }
         format.json { render json: @event, status: :created, location: @event }
       else
         format.html { render action: "new" }
@@ -62,11 +58,9 @@ class EventsController < ApplicationController
   # PUT /events/1
   # PUT /events/1.json
   def update
-    @event = Event.find(params[:id])
-
     respond_to do |format|
       if @event.update_attributes(params[:event])
-        format.html { redirect_to dwelling_event_path(@event.dwelling, @event), notice: 'Event was successfully updated.' }
+        format.html { redirect_to event_path(@event), notice: 'Event was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -78,12 +72,20 @@ class EventsController < ApplicationController
   # DELETE /events/1
   # DELETE /events/1.json
   def destroy
-    @event = Event.find(params[:id])
     @event.destroy
 
     respond_to do |format|
-      format.html { redirect_to dwelling_events_path(@event.dwelling) }
+      format.html { redirect_to events_path }
       format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def get_dwelling_and_event
+    @dwelling = current_dwelling
+    if params[:id]
+      @event = @dwelling.events.find(params[:id])
     end
   end
 end
