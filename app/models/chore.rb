9eba2b09869d9ba55ba51290885a71b62f69673d
Caleb Everett <everett.caleb@gmail.com>
2013-04-10 18:27:50 -0400

    Fix chore <-> user relation
    Relation was:
    chore `has_one :user`
    but the column in the table was assigned_user_id.
    relation changed to `belongs_to :assigned_user, :class => 'User'`

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -2,7 +2,7 @@ class Chore < ActiveRecord::Base
   attr_accessible :assigned_user_id, :description, :dwelling_id, :name
 
   belongs_to :dwelling
-  has_one :user
+  belongs_to :assigned_user, :class_name => 'User'
 
   #validates :assigned_user_id, :presence => true
   validates :dwelling_id, :presence => true
