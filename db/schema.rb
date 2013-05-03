8eaef910075fc31b2cc1c9d1ef6623f5750633ec
T Sudol <magesolumin@gmail.com>
2013-05-02 21:21:38 -0400

    Chores index only displays active chores

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130425004807) do
+ActiveRecord::Schema.define(:version => 20130428172952) do
 
   create_table "bill_payments", :force => true do |t|
     t.integer  "user_id"
